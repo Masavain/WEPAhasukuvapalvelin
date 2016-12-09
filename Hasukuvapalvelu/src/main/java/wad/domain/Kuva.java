b8f9f86aa7de309c86f119532c95309dab335c1a
Antti Koivurova <anttikoivurova@Anttis-MacBook-Pro-2.local>
2016-12-09 14:08:05 +0200

    Kuvan setterit ja getterit

--- Hasukuvapalvelu/src/main/java/wad/domain/Kuva.java
+++ Hasukuvapalvelu/src/main/java/wad/domain/Kuva.java
@@ -17,5 +17,48 @@ public class Kuva extends AbstractPersistable<Long>{
     private Kayttaja lisaaja;
     @ManyToMany
     private List<Tagays> tagaykset;
+    private String nimi;
+
+    public List<Tykkays> getTykkaukset() {
+        return tykkaukset;
+    }
+
+    public void setTykkaukset(List<Tykkays> tykkaukset) {
+        this.tykkaukset = tykkaukset;
+    }
+
+    public int getTykkayksienSumma() {
+        return tykkayksienSumma;
+    }
+
+    public void setTykkayksienSumma(int tykkayksienSumma) {
+        this.tykkayksienSumma = tykkayksienSumma;
+    }
+
+    public Kayttaja getLisaaja() {
+        return lisaaja;
+    }
+
+    public void setLisaaja(Kayttaja lisaaja) {
+        this.lisaaja = lisaaja;
+    }
+
+    public List<Tagays> getTagaykset() {
+        return tagaykset;
+    }
+
+    public void setTagaykset(List<Tagays> tagaykset) {
+        this.tagaykset = tagaykset;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+ 
+    
     
 }
