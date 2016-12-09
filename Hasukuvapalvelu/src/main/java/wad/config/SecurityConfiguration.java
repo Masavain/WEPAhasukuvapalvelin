5edb98d45ef509b3f83f6e16c4ef2fd145cc9d8b
Antti Koivurova <anttikoivurova@Anttis-MacBook-Pro-2.local>
2016-12-09 12:51:45 +0200

    Toivottavasti toimisi joskus tämä plspls

--- Hasukuvapalvelu/src/main/java/wad/config/SecurityConfiguration.java
+++ Hasukuvapalvelu/src/main/java/wad/config/SecurityConfiguration.java
@@ -5,10 +5,47 @@
  */
 package wad.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 /**
  *
  * @author anttikoivurova
  */
-public class SecurityConfiguration {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     
+    @Autowired
+    private UserDetailsService us;
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+        http
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(us).passwordEncoder(passwordEncoder());
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
